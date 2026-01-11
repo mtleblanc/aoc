fba66e036c304a45b88841fef430798d4c7547be
Michael LeBlanc <leblanc.michael@gmail.com>
2026-01-11 10:25:02 -0500

    Day 12 troll

---
+++ day12.cc
@@ -0,0 +1,86 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <machine/limits.h>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace
+{
+struct Problem
+{
+    size_t l{};
+    size_t w{};
+    std::vector<size_t> presents;
+};
+
+std::istream& operator>>(std::istream& is, Problem& n)
+{
+    is >> n.l;
+    char c {};
+    is >> c;
+    is >> n.w;
+    is >> c;
+    size_t s{};
+    while (is >> s)
+    {
+        n.presents.push_back(s);
+    }
+    return is;
+}
+
+size_t part1(std::vector<Problem>& ns)
+{
+    constexpr std::array<size_t, 6> SIZES = {7,7,7,7,6,5};
+    std::vector<size_t> fits;
+    std::ranges::transform(ns, back_inserter(fits), [&SIZES](auto &p) {
+        return std::inner_product(SIZES.begin(), SIZES.end(), p.presents.begin(), size_t{}) < p.l * p.w ? 1 : 0;
+    });
+    return std::accumulate(fits.begin(), fits.end(), size_t{});
+}
+
+size_t part2(std::vector<Problem>& ns)
+{
+    (void)ns;
+    return 0;
+}
+} // namespace
+#ifndef TESTING
+int main()
+{
+    std::ifstream fs{"input12.txt"};
+    std::vector<Problem> v;
+    std::string s;
+    for (int i = 0; i < 30; i++)
+    {
+        getline(fs, s);
+    }
+    for (; getline(fs, s);)
+    {
+        if (s.length() == 0)
+        {
+            break;
+        }
+        std::istringstream iss{s};
+        v.emplace_back();
+        try
+        {
+            iss >> v.back();
+        }
+        catch (const std::exception& ex)
+        {
+            std::cerr << ex.what() << std::endl;
+            exit(1);
+        }
+        catch (...)
+        {
+            std::cerr << "Unknown exception" << std::endl;
+        }
+    }
+
+    std::cout << part1(v) << std::endl;
+    std::cout << part2(v) << std::endl;
+}
+#endif
