4ad97d3e1c65192ed7d6271d9b9ec55cebe5cc06
Michael LeBlanc <leblanc.michael@gmail.com>
2026-01-19 15:55:53 -0500

    2015 Day 05

--- src/2015/day05.cc
+++ src/2015/day05.cc
@@ -1,5 +1,9 @@
 #include "aoc.hh"
 
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+
 /* https://adventofcode.com/2015/day/5
  */
 namespace aoc
@@ -7,9 +11,60 @@ namespace aoc
 constexpr size_t YEAR = 2015;
 constexpr size_t DAY = 5;
 
+namespace
+{
+
+bool invalidPair(std::tuple<char, char> p)
+{
+    auto [a, b] = p;
+    constexpr std::string BAD_STARTS = "acpx";
+    return BAD_STARTS.contains(a) && (b - a == 1);
+}
+
+bool isNicePart1(const std::string_view str)
+{
+    if (std::ranges::count_if(str,
+                              [](auto c)
+                              {
+                                  constexpr std::string VOWELS = "aeiou";
+                                  return VOWELS.contains(c);
+                              }) < 3)
+    {
+        return false;
+    }
+    auto adjacent = std::views::zip(str, str.substr(1));
+    return std::ranges::any_of(adjacent, [](auto p) { return get<0>(p) == get<1>(p); }) &&
+           std::ranges::none_of(adjacent, invalidPair);
+}
+
+bool containsNonOverlappingPairTwice(std::string_view str)
+{
+    while (str.size() > 3)
+    {
+        if (str.substr(2).find(str.substr(0, 2)) != std::string_view::npos)
+        {
+            return true;
+        }
+        str = str.substr(1);
+    }
+    return false;
+}
+
+bool isNicePart2(const std::string_view str)
+{
+    auto paired = std::views::zip(str, str | std::views::drop(2)) |
+                  std::views::transform([](auto p) { return get<0>(p) - get<1>(p); });
+    bool containsShiftedRepeat = std::ranges::any_of(paired, [](auto c) { return c == 0; });
+    return containsShiftedRepeat && containsNonOverlappingPairTwice(str);
+}
+
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    auto strings = readAllLines(input);
+
+    return {static_cast<size_t>(std::ranges::count_if(strings, isNicePart1)),
+            static_cast<size_t>(std::ranges::count_if(strings, isNicePart2))};
 }
 } // namespace aoc
