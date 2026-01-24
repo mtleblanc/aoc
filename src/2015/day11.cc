1fbb0b1fce20a1de4471d4e9807d81f0665e305e
Michael LeBlanc <leblanc.michael@gmail.com>
2026-01-24 00:01:59 -0500

    2015 Day 11 nonsolution

--- src/2015/day11.cc
+++ src/2015/day11.cc
@@ -9,6 +9,9 @@ constexpr size_t DAY = 11;
 
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
+    // if there isn't a pair or triple at the start of the password, the next valid
+    // will look like XXXaabcc.  This repo isn't setup to return non-size_t and the problem is
+    // solvable by inspection even without pen & paper.
     (void)input;
     return {};
 }
