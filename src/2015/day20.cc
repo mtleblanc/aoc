30f8217ccac8d9420eb23a95819f5c6465c2541d
Michael LeBlanc <leblanc.michael@gmail.com>
2026-01-31 19:41:10 -0500

    2015 Day 20 & 21

--- src/2015/day20.cc
+++ src/2015/day20.cc
@@ -1,4 +1,6 @@
 #include "aoc.hh"
+#include <algorithm>
+#include <iostream>
 
 /* https://adventofcode.com/2015/day/20
  */
@@ -7,9 +9,108 @@ namespace aoc
 constexpr size_t YEAR = 2015;
 constexpr size_t DAY = 20;
 
+namespace
+{
+struct SievePrime
+{
+    size_t next;
+    size_t prime;
+
+    SievePrime(size_t p) : next{p * 2}, prime{p} {}
+
+    void advance()
+    {
+        next += prime;
+    }
+
+    auto operator<=>(const SievePrime& o) const = default;
+};
+
+/* Sieve of Eratosthenes
+ *
+ * Maintains a heap of the next multiple of each prime so that prime factors can be read off
+ * Factors can be used to calculate d_1(n) efficiently
+ */
+struct Sieve
+{
+    std::vector<SievePrime> queue{SievePrime{2}};
+
+    size_t n{2};
+    std::vector<size_t> factors{2};
+    bool isPrime = true;
+
+    void next()
+    {
+        ++n;
+        factors.clear();
+        isPrime = false;
+        for (SievePrime* s{}; (s = &queue.front())->next == n;)
+        {
+            factors.push_back(s->prime);
+            s->advance();
+            std::ranges::pop_heap(queue, std::greater<>());
+            std::ranges::push_heap(queue, std::greater<>());
+        }
+        if (factors.empty())
+        {
+            isPrime = true;
+            factors.push_back(n);
+            queue.emplace_back(n);
+            std::ranges::push_heap(queue, std::greater<>());
+        }
+    }
+
+    // Calculate sum of divisors using the fact that if gcd(m,n)=1, d(mn) = d(m)d(n), and d(p^n) =
+    // (p^(n+1) -1)/(p-1)
+    size_t d1()
+    {
+        size_t d = 1;
+        for (auto p : factors)
+        {
+            auto pn = p;
+            while (n % (pn *= p) == 0)
+            {
+            }
+            d *= (pn - 1) / (p - 1);
+        }
+        return d;
+    }
+};
+
+// sums divisors of n whose quotient n/d <= LIM
+template <size_t LIM> size_t d1Limited(size_t n)
+{
+    size_t d{};
+    for (size_t e{1}; e <= LIM; ++e)
+    {
+        if (n % e == 0)
+        {
+            d += n / e;
+        }
+    }
+    return d;
+}
+
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    constexpr size_t P1_MULT = 10;
+    constexpr size_t P2_MULT = 11;
+    constexpr size_t P2_LIM = 50;
+    size_t target{};
+    input >> target;
+    size_t p1Target{(target + P1_MULT - 1) / P1_MULT};
+    Sieve s;
+    for (; s.d1() < p1Target; s.next())
+    {
+    }
+
+    size_t p2Target{(target + P2_MULT - 1) / P2_MULT};
+    size_t p2{p2Target / P2_LIM};
+    for (; d1Limited<P2_LIM>(p2) < p2Target; ++p2)
+    {
+    }
+    return {s.n, p2};
 }
 } // namespace aoc
--- src/2015/day21.cc
+++ src/2015/day21.cc
@@ -1,15 +1,183 @@
 #include "aoc.hh"
+#include <iostream>
+#include <scn/scan.h>
 
 /* https://adventofcode.com/2015/day/21
+
+Weapons:    Cost  Damage  Armor
+Dagger        8     4       0
+Shortsword   10     5       0
+Warhammer    25     6       0
+Longsword    40     7       0
+Greataxe     74     8       0
+
+Armor:      Cost  Damage  Armor
+Leather      13     0       1
+Chainmail    31     0       2
+Splintmail   53     0       3
+Bandedmail   75     0       4
+Platemail   102     0       5
+
+Rings:      Cost  Damage  Armor
+Damage +1    25     1       0
+Damage +2    50     2       0
+Damage +3   100     3       0
+Defense +1   20     0       1
+Defense +2   40     0       2
+Defense +3   80     0       3
+
  */
 namespace aoc
 {
 constexpr size_t YEAR = 2015;
 constexpr size_t DAY = 21;
 
+namespace
+{
+struct Item
+{
+    size_t cost;
+    int damage;
+    int armor;
+    Item(size_t cost, int damage, int armor) : cost{cost}, damage{damage}, armor{armor} {}
+};
+
+const std::vector<Item> weapons = {{8, 4, 0}, {10, 5, 0}, {25, 6, 0}, {40, 7, 0}, {74, 8, 0}};
+// include a 0 cost 0 stat item so armor is optional
+const std::vector<Item> armors = {{13, 0, 1}, {31, 0, 2},  {53, 0, 3},
+                                  {75, 0, 4}, {102, 0, 5}, {0, 0, 0}};
+// and both rings are optional
+const std::vector<Item> rings = {{25, 1, 0}, {50, 2, 0}, {100, 3, 0}, {20, 0, 1},
+                                 {40, 0, 2}, {80, 0, 3}, {0, 0, 0},   {0, 0, 0}};
+
+struct Stats
+{
+    int hp;
+    int damage;
+    int armor;
+    size_t spent{};
+    Stats(int hp, int damage, int armor) : hp{hp}, damage{damage}, armor{armor} {}
+
+    class EquipmentGuard
+    {
+        Stats* user;
+        Item item;
+
+      public:
+        EquipmentGuard(Stats& user, Item item) : user{&user}, item{item}
+        {
+            user.addStats(item);
+        }
+        EquipmentGuard(EquipmentGuard& o) = delete;
+        EquipmentGuard(EquipmentGuard&& o) = default;
+        EquipmentGuard& operator=(EquipmentGuard& o) = delete;
+        EquipmentGuard& operator=(EquipmentGuard&& o) = default;
+        ~EquipmentGuard()
+        {
+            user->removeStats(item);
+        }
+    };
+
+    EquipmentGuard equip(Item item)
+    {
+        return {*this, item};
+    }
+
+  private:
+    Stats& addStats(Item item)
+    {
+        damage += item.damage;
+        armor += item.armor;
+        spent += item.cost;
+        return *this;
+    }
+    Stats& removeStats(Item item)
+    {
+        damage -= item.damage;
+        armor -= item.armor;
+        spent -= item.cost;
+        return *this;
+    }
+};
+
+int rounds(int damage, int hp)
+{
+    if (damage <= 0)
+    {
+        damage = 1;
+    }
+    return (hp + damage - 1) / damage;
+}
+bool playerWins(Stats player, Stats monster)
+{
+    return rounds(player.damage - monster.armor, monster.hp) <=
+           rounds(monster.damage - player.armor, player.hp);
+}
+
+size_t part1(Stats monster)
+{
+    constexpr int PLAYER_HEALTH = 100;
+    size_t minCost = std::numeric_limits<size_t>::max();
+    Stats player{PLAYER_HEALTH, 0, 0};
+    for (auto weapon : weapons)
+    {
+        auto wg = player.equip(weapon);
+        for (auto armor : armors)
+        {
+            auto ag = player.equip(armor);
+            for (size_t first{}; first < rings.size(); ++first)
+            {
+                auto r1g = player.equip(rings[first]);
+                for (size_t second{first + 1}; second < rings.size(); ++second)
+                {
+                    auto r2g = player.equip(rings[second]);
+                    if (playerWins(player, monster))
+                    {
+                        minCost = std::min(minCost, player.spent);
+                    }
+                }
+            }
+        }
+    }
+    return minCost;
+}
+
+size_t part2(Stats monster)
+{
+    constexpr int PLAYER_HEALTH = 100;
+    size_t maxCost = std::numeric_limits<size_t>::min();
+    Stats player{PLAYER_HEALTH, 0, 0};
+    for (auto weapon : weapons)
+    {
+        auto wg = player.equip(weapon);
+        for (auto armor : armors)
+        {
+            auto ag = player.equip(armor);
+            for (size_t first{}; first < rings.size(); ++first)
+            {
+                auto r1g = player.equip(rings[first]);
+                for (size_t second{first + 1}; second < rings.size(); ++second)
+                {
+                    auto r2g = player.equip(rings[second]);
+                    if (!playerWins(player, monster))
+                    {
+                        maxCost = std::max(maxCost, player.spent);
+                    }
+                }
+            }
+        }
+    }
+    return maxCost;
+}
+
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+
+    auto [hp, damage, armor] =
+        scn::scan<int, int, int>(slurp(input), "Hit Points: {}\nDamage: {}\nArmor: {}\n")->values();
+    Stats monster{hp, damage, armor};
+    return {part1(monster), part2(monster)};
 }
 } // namespace aoc
