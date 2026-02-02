9c3bd6c4ca8dc135c9df76e8b64d89d6c9d9297b
Michael LeBlanc <leblanc.michael@gmail.com>
2026-02-02 10:42:43 -0500

    2016 Day 01

--- src/2016/day01.cc
+++ src/2016/day01.cc
@@ -1,4 +1,9 @@
 #include "aoc.hh"
+#include <complex>
+#include <ctre.hpp>
+#include <iostream>
+#include <ranges>
+#include <unordered_set>
 
 /* https://adventofcode.com/2016/day/1
  */
@@ -7,9 +12,65 @@ namespace aoc
 constexpr size_t YEAR = 2016;
 constexpr size_t DAY = 1;
 
+namespace
+{
+struct ComplexHash
+{
+    std::size_t operator()(const std::complex<int>& z) const noexcept
+    {
+        // NOLINTNEXTLINE
+        return z.real() ^ (z.imag() * 37);
+    }
+};
+
+size_t taxicab(std::complex<int> z)
+{
+    return std::abs(z.imag()) + std::abs(z.real());
+}
+} // namespace
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    static constexpr auto PAT = ctll::fixed_string(R"(([LR])(\d+))");
+
+    using namespace std::complex_literals;
+
+    constexpr std::complex<int> L = 1i;
+    constexpr std::complex<int> R = -1i;
+
+    auto dirs = slurp(input);
+    std::complex<int> loc;
+    std::complex<int> direction = 1i;
+    std::optional<std::complex<int>> firstRepeat;
+    std::unordered_set<std::complex<int>, ComplexHash> seen;
+    for (auto match : ctre::search_all<PAT>(dirs))
+    {
+        char dir = match.get<1>().to_view()[0];
+        if (dir == 'L')
+        {
+            direction *= L;
+        }
+        else
+        {
+            direction *= R;
+        }
+        auto steps = std::stoi(std::string(match.get<2>().to_view()));
+        for (auto _ : std::views::iota(0, steps))
+        {
+            loc += direction;
+            if (!firstRepeat)
+            {
+                if (seen.contains(loc))
+                {
+                    firstRepeat = loc;
+                }
+                else
+                {
+                    seen.insert(loc);
+                }
+            }
+        }
+    }
+
+    return {taxicab(loc), taxicab(*firstRepeat)};
 }
 } // namespace aoc
