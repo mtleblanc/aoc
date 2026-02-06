19afdb5aeaf49935f7a51aa84b95e2b9ac89c2b4
Michael LeBlanc <leblanc.michael@gmail.com>
2026-02-05 19:43:56 -0500

    2016 Day 09

--- src/2016/day09.cc
+++ src/2016/day09.cc
@@ -1,3 +1,4 @@
+
 #include "aoc.hh"
 
 /* https://adventofcode.com/2016/day/9
@@ -7,9 +8,41 @@ namespace aoc
 constexpr size_t YEAR = 2016;
 constexpr size_t DAY = 9;
 
+namespace
+{
+Solution decompress(std::istream& input, ssize_t limit = std::numeric_limits<ssize_t>::max())
+{
+    Solution sz{};
+    auto before = input.tellg();
+    for (char c{}; input.tellg() - before < limit && input >> c;)
+    {
+        if (c != '(')
+        {
+            ++sz.part1;
+            ++sz.part2;
+            continue;
+        }
+
+        ssize_t len{};
+        ssize_t rep{};
+        char c2{};
+        if (!(input >> len >> c >> rep >> c2) || c != 'x' || c2 != ')')
+        {
+            throw std::invalid_argument("bad marker");
+        }
+        sz.part2 += rep * decompress(input, len).part2;
+        sz.part1 += len * rep;
+    }
+    if (input.tellg() - before > limit)
+    {
+        throw std::invalid_argument("nested repeat overruns outer repeat length");
+    }
+    return sz;
+}
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    return decompress(input);
 }
 } // namespace aoc
