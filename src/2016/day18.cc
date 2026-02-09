14acf056765ef29d36552e9e8e4a57af83e7f164
Michael LeBlanc <leblanc.michael@gmail.com>
2026-02-09 10:45:15 -0500

    2016 Day 18

--- src/2016/day18.cc
+++ src/2016/day18.cc
@@ -1,4 +1,6 @@
 #include "aoc.hh"
+#include <algorithm>
+#include <ranges>
 
 /* https://adventofcode.com/2016/day/18
  */
@@ -7,9 +9,37 @@ namespace aoc
 constexpr size_t YEAR = 2016;
 constexpr size_t DAY = 18;
 
+namespace
+{
+template <int ROWS> int countSafe(std::string row)
+{
+    if (row.size() < 2)
+    {
+        return 0L;
+    }
+    auto accum = std::ranges::count(row, '.');
+    std::string newRow(row.size(), '.');
+    for (auto _ : std::views::iota(0, ROWS - 1))
+    {
+        newRow[0] = row[1];
+        newRow.back() = row[row.size() - 2];
+        for (auto i : std::views::iota(1U, row.size() - 1))
+        {
+            newRow[i] = row[i - 1] == row[i + 1] ? '.' : '^';
+        }
+        std::swap(row, newRow);
+        accum += std::ranges::count(row, '.');
+    }
+    return static_cast<int>(accum);
+}
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    constexpr auto PART1_ROWS = 40;
+    constexpr auto PART2_ROWS = 400000;
+    std::string row{};
+    input >> row;
+    return {countSafe<PART1_ROWS>(row), countSafe<PART2_ROWS>(row)};
 }
 } // namespace aoc
