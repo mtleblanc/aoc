12d7eac7e6f1569e700004df19a0c43d029f1b50
Michael LeBlanc <leblanc.michael@gmail.com>
2026-02-18 11:42:35 -0500

    2017 Day 05

--- src/2017/day05.cc
+++ src/2017/day05.cc
@@ -8,9 +8,45 @@ constexpr size_t YEAR = 2017;
 constexpr size_t DAY = 5;
 using Solution = Solution_t<YEAR, DAY>;
 
+namespace
+{
+auto part1(std::vector<int> offsets)
+{
+    int pc{};
+    int steps{};
+    while (0 <= pc && pc < std::ssize(offsets))
+    {
+        pc += offsets[pc]++;
+        ++steps;
+    }
+    return steps;
+}
+
+auto part2(std::vector<int> offsets)
+{
+    int pc{};
+    int steps{};
+    while (0 <= pc && pc < std::ssize(offsets))
+    {
+        auto offset = offsets[pc];
+        if (offset >= 3)
+        {
+            --offsets[pc];
+        }
+        else
+        {
+            ++offsets[pc];
+        }
+        pc += offset;
+        ++steps;
+    }
+    return steps;
+}
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    auto offsets = readAll<int>(input);
+    return {part1(offsets), part2(offsets)};
 }
 } // namespace aoc
