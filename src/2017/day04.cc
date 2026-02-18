a461e375ad6f296b756361830b0c51d770cfe585
Michael LeBlanc <leblanc.michael@gmail.com>
2026-02-17 21:28:16 -0500

    2017 Day 04

--- src/2017/day04.cc
+++ src/2017/day04.cc
@@ -1,4 +1,8 @@
 #include "aoc.hh"
+#include <algorithm>
+#include <ranges>
+#include <set>
+#include <sstream>
 
 /* https://adventofcode.com/2017/day/4
  */
@@ -10,7 +14,30 @@ using Solution = Solution_t<YEAR, DAY>;
 
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    const auto passphrases = readAllLines(input) |
+                             std::views::transform(
+                                 [](const auto& line)
+                                 {
+                                     std::stringstream ss{line};
+                                     return readAll<std::string>(ss);
+                                 }) |
+                             std::ranges::to<std::vector>();
+
+    auto allUnique = [](const auto& ps)
+    { return std::set(ps.begin(), ps.end()).size() == ps.size(); };
+
+    auto sortElements = [](const auto& r)
+    {
+        return r | std::views::transform(
+                       [](auto e)
+                       {
+                           std::ranges::sort(e);
+                           return e;
+                       });
+    };
+
+    const auto sortedPassprhases = passphrases | std::views::transform(sortElements);
+    return {static_cast<int>(std::ranges::count_if(passphrases, allUnique)),
+            static_cast<int>(std::ranges::count_if(sortedPassprhases, allUnique))};
 }
 } // namespace aoc
