1f56ebec87afca1b903fb641a49707d6a6db6d6f
mtleblanc <mtleblanc@users.noreply.github.com>
2026-02-21 00:53:54 +0000

    2017 Day 13

--- include/util.hh
+++ include/util.hh
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <charconv>
+#include <ctre.hpp>
 #include <iomanip>
 #include <ios>
+#include <ranges>
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace aoc
 {
@@ -32,10 +35,19 @@ inline std::string toHex(const auto& n, int padding = 0)
     return ss.str();
 }
 
-template <typename T> T toNum(std::string_view sv)
+template <typename T = int> T toNum(std::string_view sv)
 {
     T val{};
     std::from_chars(sv.begin(), sv.end(), val);
     return val;
+}
+
+template <typename T = int> inline std::vector<T> readNumbers(std::string_view s)
+{
+    static auto constexpr NUMS = ctll::fixed_string(R"((-?\d+))");
+    return ctre::search_all<NUMS>(s) |
+           std::views::transform([](auto m)
+                                 { return m.template get<1>().template to_number<T>(); }) |
+           std::ranges::to<std::vector>();
 }
 } // namespace aoc
--- src/2017/day13.cc
+++ src/2017/day13.cc
@@ -1,4 +1,8 @@
 #include "aoc.hh"
+#include "util.hh"
+#include <algorithm>
+#include <ranges>
+#include <stdexcept>
 
 /* https://adventofcode.com/2017/day/13
  */
@@ -8,9 +12,51 @@ constexpr size_t YEAR = 2017;
 constexpr size_t DAY = 13;
 using Solution = Solution_t<YEAR, DAY>;
 
+namespace
+{
+struct Scanner
+{
+    int layer;
+    int range;
+
+    Scanner(std::span<const int> vals)
+    {
+        if (vals.size() != 2)
+        {
+            throw std::invalid_argument("Malformed input");
+        }
+        layer = vals[0];
+        range = vals[1];
+    }
+
+    [[nodiscard]] int severity() const
+    {
+        return layer * range;
+    }
+
+    [[nodiscard]] bool caught(int delay = 0) const
+    {
+        return (layer + delay) % ((range - 1) * 2) == 0;
+    }
+};
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    auto scanners = readAllLines(input) |
+                    std::views::transform([](const auto& s) { return Scanner{readNumbers(s)}; }) |
+                    std::ranges::to<std::vector>();
+
+    auto part1 = std::ranges::fold_left(
+        scanners | std::views::filter([](const auto& s) { return s.caught(); }) |
+            std::views::transform([](const auto& s) { return s.severity(); }),
+        0, std::plus<>());
+
+    auto escape = [&scanners](auto n)
+    { return std::ranges::none_of(scanners, [n](const auto& s) { return s.caught(n); }); };
+    auto searchSpace = std::views::iota(0);
+    auto part2 = std::ranges::find_if(searchSpace, escape);
+
+    return {part1, *part2};
 }
 } // namespace aoc
