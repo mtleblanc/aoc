dabd231241ffd064b5b2558a60d7a50ef2298e91
mtleblanc <mtleblanc@users.noreply.github.com>
2026-02-23 17:56:16 +0000

    2017 - Day 18

--- src/2017/day18.cc
+++ src/2017/day18.cc
@@ -1,6 +1,7 @@
 #include "aoc.hh"
 #include <deque>
 #include <stdexcept>
+#include <utility>
 #include <vector>
 
 /* https://adventofcode.com/2017/day/18
@@ -65,19 +66,17 @@ struct Machine
     std::vector<Instruction> program;
     std::deque<register_t> sent;
     std::deque<register_t>* receiveQueue{};
-    std::vector<register_t> received;
+    std::optional<register_t> recovered;
     bool blocked{};
+    int sentCount{};
     ssize_t ip{};
 
-    Machine(const std::vector<Instruction>& program) : program{program} {}
-    Machine(std::vector<Instruction>&& program) : program{std::move(program)} {}
+    Machine(std::vector<Instruction> program) : program{std::move(program)} {}
 
     register_t& reg(char r)
     {
         return registers.at(r - 'a');
     }
-
-    void optimize();
 
     void step()
     {
@@ -97,18 +96,6 @@ struct Machine
         {
             step();
         }
-    }
-
-    void reset()
-    {
-        for (auto& r : registers)
-        {
-            r = 0;
-        }
-        sent.clear();
-        received.clear();
-        receiveQueue = nullptr;
-        ip = 0;
     }
 };
 
@@ -130,6 +117,7 @@ void Instruction::apply(Machine& cm) const
     switch (t)
     {
     case Type::SND:
+        ++cm.sentCount;
         cm.sent.push_back(first.value(cm));
         break;
     case Type::SET:
@@ -149,27 +137,27 @@ void Instruction::apply(Machine& cm) const
         first.reference(cm) %= second.value(cm);
         break;
     case Type::RCV:
-        if (first.value(cm) != 0)
+        if (cm.receiveQueue == nullptr)
         {
-            if (cm.receiveQueue == nullptr)
+            if (first.value(cm) != 0)
             {
                 if (cm.sent.empty())
                 {
                     throw std::invalid_argument("Receive before send");
                 }
-                cm.received.push_back(cm.sent.back());
+                cm.recovered = cm.sent.back();
             }
-            else
+        }
+        else
+        {
+            if (cm.receiveQueue->empty())
             {
-                if (cm.receiveQueue->empty())
-                {
-                    cm.blocked = true;
-                    return;
-                }
-                cm.blocked = false;
-                cm.received.push_back(cm.receiveQueue->front());
-                cm.receiveQueue->pop_front();
+                cm.blocked = true;
+                return;
             }
+            cm.blocked = false;
+            first.reference(cm) = cm.receiveQueue->front();
+            cm.receiveQueue->pop_front();
         }
         break;
     case Type::JGZ:
@@ -253,7 +241,7 @@ template <> Solution solve<YEAR, DAY>(std::istream& input)
     using namespace Interpreter;
     auto program = readAll<Instruction>(input);
     auto m0 = Machine{program};
-    while (m0.received.empty())
+    while (!m0.recovered)
     {
         m0.step();
         if (m0.blocked)
@@ -261,18 +249,25 @@ template <> Solution solve<YEAR, DAY>(std::istream& input)
             throw std::invalid_argument("Program ended before receiving");
         }
     }
-    int part1 = static_cast<int>(m0.received.front());
-    m0.reset();
+    int part1 = static_cast<int>(*m0.recovered);
+    m0 = Machine{program};
     auto m1 = Machine{std::move(program)};
     m1.reg('p') = 1;
     m1.receiveQueue = &m0.sent;
     m0.receiveQueue = &m1.sent;
-    while (!m0.blocked || !m1.blocked)
+
+    auto* running = &m0;
+    auto* waiting = &m1;
+    while (!running->blocked)
     {
-        m0.step();
-        m1.step();
+        running->step();
+        if (running->blocked)
+        {
+            std::swap(running, waiting);
+            running->step();
+        }
     }
-    int part2 = static_cast<int>(std::ssize(m1.sent) + std::ssize(m0.received));
+    int part2 = m1.sentCount;
     return {part1, part2};
 }
 } // namespace aoc
