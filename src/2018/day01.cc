fce3a0c5a1be84ca9675de2991696be16aae1b58
mtleblanc <mtleblanc@users.noreply.github.com>
2026-02-27 17:43:08 +0000

    2018 - Day 01

--- src/2018/day01.cc
+++ src/2018/day01.cc
@@ -1,4 +1,6 @@
 #include "aoc.hh"
+#include <algorithm>
+#include <set>
 
 /* https://adventofcode.com/2018/day/1
  */
@@ -8,9 +10,31 @@ constexpr size_t YEAR = 2018;
 constexpr size_t DAY = 1;
 using Solution = Solution_t<YEAR, DAY>;
 
+namespace
+{
+auto part2(const auto& deltas)
+{
+    auto total = 0;
+    auto seen = std::set<int>{};
+    for (;;)
+    {
+        for (auto d : deltas)
+        {
+            total += d;
+            if (seen.contains(total))
+            {
+                return total;
+            }
+            seen.insert(total);
+        }
+    }
+}
+} // namespace
+
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    auto deltas = readAll<int>(input);
+    auto part1 = std::ranges::fold_left(deltas, 0, std::plus<>());
+    return {part1, part2(deltas)};
 }
 } // namespace aoc
