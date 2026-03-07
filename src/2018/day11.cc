93cbcf0812ae4eab877b6671f63226681afad140
mtleblanc <mtleblanc@users.noreply.github.com>
2026-03-07 23:56:05 +0000

    2018 - Day 11 - more cleanup

--- src/2018/day11.cc
+++ src/2018/day11.cc
@@ -20,9 +20,9 @@ namespace
 auto powerLevel(auto x, auto y, auto seed)
 {
     // NOLINTBEGIN(*-magic-numbers)
-    auto rackId = x + 10;
-    auto powerLevel = (rackId * y + seed) * rackId;
-    return (powerLevel / 100) % 10 - 5;
+    auto rack = x + 10;
+    auto pl = (rack * y + seed) * rack;
+    return (pl / 100) % 10 - 5;
     // NOLINTEND(*-magic-numbers)
 }
 
@@ -48,19 +48,7 @@ auto makePartials(auto seed)
 
 auto optimize(const auto& partials)
 {
-    auto coords =
-        std::views::cartesian_product(std::views::iota(1, GRID + 1), std::views::iota(1, GRID + 1));
-    auto squares =
-        coords |
-        std::views::transform(
-            [](auto c)
-            {
-                auto m = std::max(std::get<0>(c), std::get<1>(c));
-                return std::views::iota(0, GRID + 1 - m) |
-                       std::views::transform([c](auto n) { return std::make_pair(c, n); });
-            }) |
-        std::views::join;
-
+    using namespace std::views;
     auto powerAt = [&partials](auto s)
     {
         // includsion exclusion:
@@ -71,24 +59,27 @@ auto optimize(const auto& partials)
         //   CDDDD
         // sum of D is sum of all values, minus the values left (C + B), minus the values above (A +
         // B), but B has been subtracted twice, so need to add back once
-        auto [c, n] = s;
-        auto [x, y] = c;
+        auto [x, y, n] = s;
         return partials[x + n - 1][y + n - 1] + partials[x - 1][y - 1] -
                partials[x - 1][y + n - 1] - partials[x + n - 1][y - 1];
     };
 
     StringSolution res;
     {
-        auto sizeThree = squares | std::views::filter([](auto s) { return s.second == 3; });
-        auto best = std::ranges::max(sizeThree, {}, powerAt);
-        auto [c, n] = best;
-        auto [x, y] = c;
+        // NB GRID - 1, max top left corner for a 3x3 = GRID - 2
+        auto squares = cartesian_product(iota(1, GRID - 1), iota(1, GRID - 1), single(3));
+        auto [x, y, n] = std::ranges::max(squares, {}, powerAt);
         res.part1 = std::format("{},{}", x, y);
     }
     {
-        auto best = std::ranges::max(squares, {}, powerAt);
-        auto [c, n] = best;
-        auto [x, y] = c;
+        auto valid = [](auto square)
+        {
+            auto [x, y, n] = square;
+            return x + n <= GRID + 1 && y + n <= GRID + 1;
+        };
+        auto squares = cartesian_product(iota(1, GRID + 1), iota(1, GRID + 1), iota(1, GRID + 1)) |
+                       filter(valid);
+        auto [x, y, n] = std::ranges::max(squares, {}, powerAt);
         res.part2 = std::format("{},{},{}", x, y, n);
     }
     return res;
