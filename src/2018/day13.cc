6368fd811366468e437331656dafb03236632930
mtleblanc <mtleblanc@users.noreply.github.com>
2026-03-11 20:33:58 +0000

    2018 - Day 13

--- src/2018/day13.cc
+++ src/2018/day13.cc
@@ -2,6 +2,7 @@
 #include "coords.hh"
 #include <algorithm>
 #include <ranges>
+#include <utility>
 
 /* https://adventofcode.com/2018/day/13
  */
@@ -18,11 +19,72 @@ using Solution = Solution_t<YEAR, DAY>;
 
 namespace
 {
+struct Direction
+{
+
+    enum class Turn
+    {
+        LEFT,
+        STRAIGHT,
+        RIGHT,
+        REVERSE,
+        COUNT
+    };
+
+    // y,x indexing so that sort is reading order
+    static constexpr auto UP = std::make_pair(-1, 0);
+    static constexpr auto LEFT = std::make_pair(0, -1);
+    static constexpr auto RIGHT = std::make_pair(0, 1);
+    static constexpr auto DOWN = std::make_pair(1, 0);
+
+    static constexpr auto doTurn(std::pair<int, int> direction, Turn turn) -> std::pair<int, int>
+    {
+        auto [y, x] = direction;
+        switch (turn)
+        {
+        case Turn::LEFT:
+            return {-x, y};
+        case Turn::STRAIGHT:
+            return direction;
+        case Turn::RIGHT:
+            return {x, -y};
+        case Turn::REVERSE:
+            return {-y, -x};
+        case Turn::COUNT:
+            throw std::logic_error{"Invalid turn value"};
+        default:
+            std::unreachable();
+        }
+    }
+
+    static constexpr auto doCorner(std::pair<int, int> direction, char c) -> std::pair<int, int>
+    {
+        if (c != '\\' && c != '/')
+        {
+            throw std::invalid_argument{"Invalid corner"};
+        }
+        auto [y, x] = direction;
+        if (c == '\\')
+        {
+            return {x, y};
+        }
+        return {-x, -y};
+    }
+};
+
 struct Cart
 {
     std::pair<int, int> loc;
     std::pair<int, int> dir;
-    int turn = -1;
+    int turnIndex = 0;
+    bool alive = true;
+    constexpr static std::array<Direction::Turn, 3> TURNS = {
+        {Direction::Turn::LEFT, Direction::Turn::STRAIGHT, Direction::Turn::RIGHT}};
+
+    [[nodiscard]] Direction::Turn turn() const
+    {
+        return TURNS[turnIndex % std::ssize(TURNS)];
+    }
 
     auto operator<=>(const Cart&) const = default;
 };
@@ -73,17 +135,41 @@ class Maze
     }
 };
 
-auto firstCrash(const auto& maze, auto carts)
+auto simulate(const auto& maze, auto carts)
 {
-    std::ranges::sort(carts);
+    auto found = false;
+    auto res = StringSolution{};
     for (;;)
     {
+        std::ranges::sort(carts);
+        carts = std::ranges::to<std::vector>(carts |
+                                             std::views::filter([](auto& c) { return c.alive; }));
+        if (carts.size() == 1)
+        {
+            auto [y, x] = carts.front().loc;
+            res.part2 = std::format("{},{}", x, y);
+            return res;
+        }
         for (auto& c : carts)
         {
-            auto nextLoc = c.loc + c.dir;
-            if (std::ranges::any_of(carts, [=](auto c) { return c.loc == nextLoc; }))
+            if (!c.alive)
             {
-                return std::format("{},{}", nextLoc.second, nextLoc.first);
+                continue;
+            }
+            auto nextLoc = c.loc + c.dir;
+            // because we check on each update, at most one crash
+            auto crash =
+                std::ranges::find_if(carts, [=](auto c) { return c.alive && c.loc == nextLoc; });
+            if (crash != carts.end())
+            {
+                if (!found)
+                {
+                    found = true;
+                    res.part1 = std::format("{},{}", nextLoc.second, nextLoc.first);
+                }
+                c.alive = false;
+                crash->alive = false;
+                continue;
             }
             c.loc = nextLoc;
             switch (maze[c.loc])
@@ -91,32 +177,16 @@ auto firstCrash(const auto& maze, auto carts)
             case '|':
             case '-':
                 break;
+            case '\\':
             case '/':
             {
-                auto [y, x] = c.dir;
-                c.dir = std::make_pair(x, y);
-                break;
-            }
-            case '\\':
-            {
-                auto [y, x] = c.dir;
-                c.dir = std::make_pair(-x, -y);
+                c.dir = Direction::doCorner(c.dir, maze[c.loc]);
                 break;
             }
             case '+':
             {
-                if (c.turn == 0)
-                {
-                    ++c.turn;
-                    break;
-                }
-                auto [y, x] = c.dir;
-                c.dir = std::make_pair(-c.turn * x, c.turn * y);
-                ++c.turn;
-                if (c.turn == 2)
-                {
-                    c.turn -= 2;
-                }
+                c.dir = Direction::doTurn(c.dir, c.turn());
+                ++c.turnIndex;
                 break;
             }
             default:
@@ -136,19 +206,19 @@ template <> Solution solve<YEAR, DAY>(std::istream& input)
         {
         case '^':
             maze[i] = '|';
-            carts.emplace_back(maze.index(i), std::make_pair(-1, 0));
+            carts.emplace_back(maze.index(i), Direction::UP);
             break;
         case '<':
             maze[i] = '-';
-            carts.emplace_back(maze.index(i), std::make_pair(0, -1));
+            carts.emplace_back(maze.index(i), Direction::LEFT);
             break;
         case 'v':
             maze[i] = '|';
-            carts.emplace_back(maze.index(i), std::make_pair(1, 0));
+            carts.emplace_back(maze.index(i), Direction::DOWN);
             break;
         case '>':
             maze[i] = '-';
-            carts.emplace_back(maze.index(i), std::make_pair(0, 1));
+            carts.emplace_back(maze.index(i), Direction::RIGHT);
             break;
         case '+':
         case '-':
@@ -162,6 +232,6 @@ template <> Solution solve<YEAR, DAY>(std::istream& input)
             throw std::invalid_argument{"Unrecognized character"};
         }
     }
-    return {firstCrash(maze, carts)};
+    return simulate(maze, carts);
 }
 } // namespace aoc
