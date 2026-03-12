df48cf7d504d932bb17f77edea082c130354d123
mtleblanc <mtleblanc@users.noreply.github.com>
2026-03-12 21:05:17 +0000

    2018 - Day 14 - custom view

--- src/2018/day14.cc
+++ src/2018/day14.cc
@@ -1,6 +1,8 @@
 #include "aoc.hh"
 #include "util.hh"
 #include <algorithm>
+#include <iterator>
+#include <print>
 #include <ranges>
 
 /* https://adventofcode.com/2018/day/14
@@ -20,7 +22,7 @@ namespace
 {
 constexpr auto B = 10;
 
-struct Scoreboard
+class Scoreboard : std::ranges::view_interface<Scoreboard>
 {
     // NOLINTNEXTLINE(*-magic-numbers)
     std::vector<int> scoreboard{3, 7};
@@ -39,38 +41,99 @@ struct Scoreboard
         elf1 = (elf1 + 1 + scoreboard[elf1]) % std::ssize(scoreboard);
         elf2 = (elf2 + 1 + scoreboard[elf2]) % std::ssize(scoreboard);
     }
+
+  public:
+    class Iterator
+    {
+        Scoreboard* sb;
+        int64_t index = 0;
+        Iterator(Scoreboard* sb) : sb{sb} {}
+        friend class Scoreboard;
+
+      public:
+        using difference_type = int64_t;
+        using value_type = int;
+        using reference = int;
+        using iterator_category = std::random_access_iterator_tag;
+        auto operator*() const
+        {
+            while (std::ssize(sb->scoreboard) <= index)
+            {
+                sb->makeRecipes();
+            }
+            return sb->scoreboard[index];
+        }
+
+        auto& operator++()
+        {
+            ++index;
+            return (*this);
+        }
+
+        auto operator++(int)
+        {
+            auto res = *this;
+            return ++res;
+        }
+
+        auto& operator--()
+        {
+            --index;
+            return (*this);
+        }
+
+        auto operator--(int)
+        {
+            auto res = *this;
+            return --res;
+        }
+
+        auto operator-(Iterator o) const
+        {
+            return index - o.index;
+        }
+
+        auto operator<=>(const Iterator& o) const
+        {
+            return index <=> o.index;
+        }
+    };
+
+    Scoreboard() = default;
+    Scoreboard(const Scoreboard&) = delete;
+    Scoreboard(Scoreboard&&) = default;
+    Scoreboard& operator=(const Scoreboard&) = delete;
+    Scoreboard& operator=(Scoreboard&&) = default;
+    ~Scoreboard() = default;
+
+    auto begin()
+    {
+        return Iterator{this};
+    }
+
+    static auto end()
+    {
+        return std::unreachable_sentinel;
+    }
 };
 
-// This could be done as part of part2, but practice is on the order of 70k while part2 returns an
-// answer on the order of 20m, so the duplicate work is immaterial
-auto part1(auto practice)
+auto part1(auto practice, auto& scoreboard)
 {
     static constexpr auto N = 10;
-    auto scoreboard = Scoreboard{};
-    while (std::ssize(scoreboard.scoreboard) < practice + N)
-    {
-        scoreboard.makeRecipes();
-    }
-    auto resScores = scoreboard.scoreboard | std::views::drop(practice) | std::views::take(N);
+    auto resScores = scoreboard | std::views::drop(practice) | std::views::take(N);
     return std::ranges::fold_left(resScores, int64_t{0},
                                   [](auto acc, auto n) { return B * acc + n; });
 }
 
-auto part2(const auto& target)
+auto part2(const auto& target, auto& scoreboard)
 {
-    auto scoreboard = Scoreboard{};
-    auto index = int64_t{0};
-    for (;;)
+    auto it = scoreboard.begin();
+
+    for (;; ++it)
     {
-        scoreboard.makeRecipes();
-        while (index + std::ssize(target) < std::ssize(scoreboard.scoreboard))
+        if (std::ranges::equal(target, std::views::counted(it, std::ssize(target))))
         {
-            if (std::ranges::equal(target, scoreboard.scoreboard | std::views::drop(index) |
-                                               std::views::take(std::ssize(target))))
-            {
-                return index;
-            }
-            ++index;
+            return it - scoreboard.begin();
         }
     }
 }
@@ -83,6 +146,7 @@ template <> Solution solve<YEAR, DAY>(std::istream& input)
     auto practice = toNum(target);
     auto targetVec = std::ranges::to<std::vector>(
         target | std::views::transform([](auto c) { return c - '0'; }));
-    return {part1(practice), part2(targetVec)};
+    auto scoreboard = Scoreboard{};
+    return {part1(practice, scoreboard), part2(targetVec, scoreboard)};
 }
 } // namespace aoc
